7b1f44daae4d18d1038f4be70564ed2916662220
Alexhhh <42209822+Alexhhhc@users.noreply.github.com>
2021-07-19 11:34:18 +0800

    init

---
+++ MultiOnlineBags.py
@@ -0,0 +1,189 @@
+import mc
+import json
+import pymongo
+import os
+import re #处理字符串中的数字用的
+
+# 从文件中获取JSON
+def fileConvert(file):
+    with open( file, 'r' ) as f:
+        content = f.read()
+    return content
+class playerClass(): 
+    # 注意Data是json格式，oganesson和alexhhh都是伞兵
+    def __init__(self, player):
+        # 初始化玩家对应的数据库
+        address = "localhost:27017/"
+        client = pymongo.MongoClient("mongodb://" + address)
+        db = client['bagsDB']
+        self.col = db[player.xuid]
+        self.player = player
+        self.totalBagNum = 2 #为将来玩家背包数不同铺路
+        self.getNonFreeBags()
+        self.getFreeBags()
+    def delAllItem(self):
+        # 删除游戏中数据，通过调用写入实现
+        Data = fileConvert("defaultBag.json")
+        self.player.setAllItem(Data)
+    def getDbItem(self, bagNum):
+        # 从数据库中获取背包
+        Data = self.col.find_one({"_id": bagNum})
+        del Data["_id"]
+        DataJson = json.dumps(Data)
+        self.Data = DataJson
+    def setDbItem(self, bagNum):
+        # 将背包放入数据库
+        DataDict = json.loads(self.Data)
+        DataDict["_id"] = bagNum
+        #Data = json.dumps(DataDict)
+        self.col.insert_one(DataDict)
+    def delDbItem(self, bagNum):
+        self.col.delete_one( { "_id": bagNum })        
+    def getNonFreeBags(self):
+        # 获取非空背包位置,这里的空和非空是对于MongoDB数据库而言
+        self.nonFreeBagNumList = []
+        for data in self.col.find({},{"_id": 1 }):
+            self.nonFreeBagNumList.append(data["_id"])
+        self.nonFreeBagNumList.sort()
+    def getFreeBags(self):
+        # 获取空背包位置
+        self.allBagNumList = (1,self.totalBagNum)
+        self.freeBagNumList = [numx for numx in self.allBagNumList if numx not in self.nonFreeBagNumList]
+        self.freeBagNumList.sort()
+    def newForm(self, mode):
+        # 调用form类，为玩家显示窗体
+        self.form = form(mode, self.player, self)
+        self.form.push()
+    def get(self, arg):
+        # 执行get操作
+        if json.loads(str(self.player.getAllItem())) == json.loads(str(fileConvert("defaultBag.json"))):
+            self.getDbItem(self.nonFreeBagNumList[arg])
+            self.player.setAllItem(self.Data) # self.data就是json格式
+            self.delDbItem(self.nonFreeBagNumList[arg])
+            mc.logout("name:"+str(self.player.name)+" xuid:"+str(self.player.xuid)+" 的背包获取完毕\n")
+            code = 0
+        else:
+            code = 10086
+        return code
+    def save(self, arg):
+        #执行save操作
+        self.Data = self.player.getAllItem()
+        if self.freeBagNumList != []:
+            self.setDbItem(self.freeBagNumList[arg])
+            self.delAllItem()
+        mc.logout("name:"+str(self.player.name)+" xuid:"+str(self.player.xuid)+" 的背包保存完毕\n")
+    def getBagList(self, bagNumList):
+        bagList = "["
+        for bagNum in bagNumList:
+            bagList = bagList + json.dumps({'text':str(bagNum)}) + ","
+        bagList = bagList[:-1] + "]"
+        return bagList
+
+class form():
+    def __init__(self, mode, player, playerObject):
+        # 将外参转变为内......参（字面意思
+        self.mode = mode
+        self.playerObject = playerObject
+        self.player = player
+    def push(self):
+        self.playerObject.getNonFreeBags()
+        self.playerObject.getFreeBags()
+        if self.mode == "main":
+            self.listId = self.player.sendSimpleForm('MultiOnlineBags', '请选择:', '[{"text":"保存当前背包"},{"text":"获取背包"},{"text":"取消"}]')
+        elif self.mode == "get":
+            bagList = self.playerObject.getBagList(self.playerObject.nonFreeBagNumList)
+            if self.playerObject.nonFreeBagNumList == []:
+                self.listId = self.player.sendSimpleForm('MultiOnlineBags', '当前没有可以取用的背包|･ω･｀)', '[{"text":"返回"}]')
+            else:
+                self.listId = self.player.sendSimpleForm('MultiOnlineBags', '请选择取用的背包\n当前可用背包:', str(bagList))
+        elif self.mode == "save":
+            bagList = self.playerObject.getBagList(self.playerObject.freeBagNumList)
+            if self.playerObject.freeBagNumList == []:
+                self.listId = self.player.sendSimpleForm('MultiOnlineBags', '数据库中你的背包已经存满w(ﾟДﾟ)w', '[{"text":"返回"}]')
+            else:
+                self.listId = self.player.sendSimpleForm('MultiOnlineBags', '请选择存储的背包\n当前可用背包:', str(bagList))
+        elif self.mode == "get_pass":
+            self.listId = self.player.sendSimpleForm('MultiOnlineBags', '取出成功 (ﾉ*･ω･)ﾉ', '[{"text":"返回主菜单"},{"text":"完成"}]')
+        elif self.mode == "save_pass":
+            self.listId = self.player.sendSimpleForm('MultiOnlineBags', '存入成功 =￣ω￣=', '[{"text":"返回主菜单"},{"text":"完成"}]')
+        elif self.mode == "get_not_empty":
+            self.listId = self.player.sendSimpleForm('MultiOnlineBags', '您当前身上存在物品，无法取出，请清除身上物品后重试', '[{"text":"完成"}]')
+        elif self.mode == "incorrect":
+            self.player.sendTextPacket("[MOB]您输入的命令有误，我们提供了/bags，/bags save与/bags get以更改您的背包")
+        global listDict
+        listDict[int(self.listId)] = self
+    def execute(self, arg):
+        if self.mode == "main":
+            if arg == 0:
+                self.playerObject.newForm("save")
+            if arg == 1:
+                self.playerObject.newForm("get")
+        elif self.mode == "get":
+            code = self.playerObject.get(arg)
+            if code == 0:
+                self.playerObject.newForm("get_pass")
+            elif code == 10086:
+                self.playerObject.newForm("get_not_empty")
+        elif self.mode == "save":
+            self.playerObject.save(arg)
+            self.playerObject.newForm("save_pass")
+        elif self.mode == "save_pass":  
+            if arg == 0:
+                self.playerObject.newForm("main")
+        elif self.mode == "get_pass":
+            if arg == 0:
+                self.playerObject.newForm("main")
+
+def command(arg):
+    player = arg['player']
+    cmd = arg['cmd'].split()
+    global playerObject
+    if cmd[0] == "/bags":
+        if len(cmd) == 1:
+            playerObject[player].newForm("main")
+        elif cmd[1] == 'get':
+            playerObject[player].newForm("get")
+        elif cmd[1] == 'save':
+            playerObject[player].newForm("save")
+        else:
+            playerObject[player].newForm("incorrect")
+
+def replyForm(arg):
+    if arg['selected'] != 'null':
+        global playerObject
+        formid = arg['formid']
+        playerObject[arg['player']].form.execute(int(arg['selected']))
+
+def join(player):
+    global playerObject
+    playerObject[player] = playerClass(player) #将playerClass实例化，从而调用属性和方法
+    if playerObject[player].nonFreeBagNumList == []:
+        player.sendTextPacket("[MOB]你是第一次登录服务器耶！\n如果不是，请向腐竹反馈")
+    else:
+        playerObject[player].get(0)
+
+def left(player):
+    global playerObject
+    playerObject[player].getNonFreeBags()
+    playerObject[player].getFreeBags()
+    playerObject[player].save(0)
+    del playerObject[player]
+
+listDict = {}
+playerObject = {}
+#defaultTotalBagNum = 2
+
+# 注册指令
+mc.setCommandDescription('bags','背包同步')
+
+# 进入监听器
+mc.setListener('onPlayerJoin', join)
+
+# 退出监听器
+mc.setListener('onPlayerLeft', left)
+
+# 命令监听器
+mc.setListener('onInputCommand', command)
+
+# 表单监视器
+mc.setListener('onSelectForm', replyForm)
---
+++ README.md
@@ -0,0 +1,67 @@
+## 简介
+
+以MongoDB数据库存储背包数据，在多个minecraft BDS服务端（即官方基岩版服务端）之间同步玩家的背包、穿戴、主手物品、末影箱等数据，从而无缝衔接游戏体验。可跨服务器同步。
+
+在BDS 1.17.2上测试通过。由于BDSpyrunner上游原因，暂未支持BDS 1.17.10。
+
+这个插件依赖于：
+
+- MongoDB服务端
+- Python3.7（未测试在其他版本上的可行性）
+- liteloader BDS（<1.0.3）
+- BDSpyrunner
+- pymongo python模块
+
+## 部署
+
+### Windows7/10/11
+
+现在假定你已经将minecraft BDS安装到了硬盘的某一位置。
+
+1. 在[Index of /ftp/python/](https://www.python.org/ftp/python/)下载以3.7开头的python-3.7.x-amd64.exe，安装，注意安装的时候**一定一定一定**要勾选添加PATH。
+2. 在[MongoDB Community Download | MongoDB](https://www.mongodb.com/try/download/community)下载mongodb，选择Cloud>MongoDB Community Server，version选最新稳定版，Platform选windows，Package选msi，下载安装。（网页可能显示不全）
+3. 在https://github.com/LiteLDev/LiteLoaderBDS/releases/页面下载lightloader，注意暂不能使用1.0.3及更高版本，因为没适配。下载后把解压出的所有文件放入BDS根目录，运行RoDB.exe文件。
+4. 在[Actions · twoone-3/BDSpyrunner (github.com)](https://github.com/twoone-3/BDSpyrunner/actions)页面找“x.x.x更新”开头的workflow，下载其中artifacts里的dll文件。将dll文件放入BDS根目录中的plugins文件夹。
+5. 运行BDS主程序，如果出现BDSpyrunner说明配置成功。
+6. 下载release里的压缩包，将其中的MultiOnlineBags.py放入plugins/py文件夹，defaultBag.json放入BDS根目录。
+7. 打开powershell（或者命令提示符），执行pip install pymongo --target=plugins/py文件夹的路径
+8. 在硬盘某一位置建立一个文件夹。（该目录和上级目录最好都是英文）
+9. 在mongodb的bin目录（默认在C:\Program Files\MongoDB\Server\\[版本号]\bin）shift右键，用powershell（命令提示符也行）执行.\mongod --dbpath 刚才的文件夹目录，并保持这个终端窗口不要关闭。
+10. 打开BDS，看到 loading MultiOnlineBags 应该就可以使用了
+
+## 配置
+
+在MultiOnlineBags.py上直接修改：
+
+```python
+class playerClass(): 
+    def __init__(self, player):
+        address = "localhost:27017/" #可修改，写成你的MongoDB数据库ip:端口
+        client = pymongo.MongoClient("mongodb://" + address)
+        db = client['bagsDB']
+        self.col = db[player.xuid]
+        self.player = player
+        self.totalBagNum = 2 #可修改，每个玩家拥有的背包个数
+        self.getNonFreeBags()
+        self.getFreeBags()
+```
+
+## 问题排查与避免
+
+- 玩家进入后，BDS卡死，退出后过一段时间滚出大堆错误：MongoDB server没启动，或者ip/端口配置出错
+- 每个玩家进入时都会提示是第一次进入：刚装插件后第一次进提示是正常现象，每次进入都提示，请发issue
+- 弹出对话框提示XXX dll出错：可能是按照BDSpyrunner装了最新版的BDXcore而不是liteloader，wiki害人呐
+- 其他问题，包括README看不懂，操作卡在哪一步之类，发issue就行。
+
+多bb一句，虽说突然关服应该不会丢失玩家背包数据，关服前还是让所有玩家先自行退出为妙。~~可以直接kick @a~~
+
+上插件之前最好也做一次备份，避免背包数据丢失。
+
+## 鸣谢
+
+感谢oganesson不懈（麻了/懂了）地修改代码（大部分代码是他写的）
+
+感谢俺妹（lighthua）维护了一个优秀的服务器，给我们写插件带来了动力
+
+（加QQ群1101450234，欢迎来玩）
+
